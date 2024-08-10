953ee5e1d9bcb0cba9256ac59d744493f8c88d44
Amir <amir@amir>
2024-08-10 01:40:10 -0400

    build: add project structure.

---
+++ authapp/api/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import include, path
+
+# urlpatterns = [
+#     path("auth/", include(("authapp.authentication.urls", "authentication"))),
+#     path("users/", include(("authapp.users.urls", "users"))),
+# ]
---
+++ config/asgi.py
@@ -0,0 +1,20 @@
+"""
+ASGI config for authapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+from config.env import BASE_DIR, env
+
+env.read_env(os.path.join(BASE_DIR, ".env"))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.django.base")
+
+application = get_asgi_application()
